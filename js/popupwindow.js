776359def9e6f042bafee9829a52333ba7d36e35
kiio1224 <92066969+kiio1224@users.noreply.github.com>
2022-12-08 12:54:37 +0900

    a

--- js/popupwindow.js
+++ js/popupwindow.js
@@ -1,17 +1,44 @@
-
-var modal = document.querySelector("#modal");
-var modalOverlay = document.querySelector("#modal-overlay");
-var closeButton = document.querySelector("#close-button");
-var openButton = document.querySelector("#open-button");
-
-//閉じるボタン
-closeButton.addEventListener("click", function () {
-  modal.classList.toggle("closed");
-  modalOverlay.classList.toggle("closed");
+const modal1 = document.querySelector('.modal1');
+const modal2 = document.querySelector('.modal2');
+const modal3= document.querySelector('.modal3');
+const closeBtn = document.querySelector('.modal__close');
+const overlay = document.querySelector('.overlay');
+// const btn = document.querySelectorAll('.demo__circle');
+const btn1 = document.querySelector('.demo__color1');
+const btn2 = document.querySelector('.demo__color2');
+const btn3 = document.querySelector('.demo__color3');
+//for(let i; i<=btn.length;i++){
+  btn1.addEventListener('click',function(e){
+    // aタグのデフォルトの機能を停止する
+    e.preventDefault();
+    // モーダルとオーバーレイにactiveクラスを付与する
+    modal1.classList.add('active');
+    overlay.classList.add('active');
+    // モダルの閉じるボタンをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+closeBtn.addEventListener('click', function(){
+  modal1.classList.remove('active');
+  overlay.classList.remove('active');
 });
 
-//開くボタン
-openButton.addEventListener("click", function () {
-  modal.classList.toggle("closed");
-  modalOverlay.classList.toggle("closed");
+// オーバーレイをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+overlay.addEventListener('click', function() {
+  modal1.classList.remove('active');
+  overlay.classList.remove('active');
 });
+})
+// btn1.addEventListener('click', function(e){
+
+//   e.preventDefault();
+//   modal1.classList.add('active');
+//   overlay.classList.add('active');
+// });
+// closeBtn.addEventListener('click', function(){
+//   modal1.classList.remove('active');
+//   overlay.classList.remove('active');
+// });
+// overlay.addEventListener('click', function() {
+//   modal1.classList.remove('active');
+//   overlay.classList.remove('active');
+// });
+//
+//
