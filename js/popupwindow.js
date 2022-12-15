5d6f2d41ff5c220918a2cc9b73a300ab55ebb16b
kiio1224 <92066969+kiio1224@users.noreply.github.com>
2022-12-15 12:33:05 +0900

    1215

--- js/popupwindow.js
+++ js/popupwindow.js
@@ -1,44 +1,31 @@
-const modal1 = document.querySelector('.modal1');
-const modal2 = document.querySelector('.modal2');
-const modal3= document.querySelector('.modal3');
-const closeBtn = document.querySelector('.modal__close');
-const overlay = document.querySelector('.overlay');
-// const btn = document.querySelectorAll('.demo__circle');
-const btn1 = document.querySelector('.demo__color1');
-const btn2 = document.querySelector('.demo__color2');
-const btn3 = document.querySelector('.demo__color3');
-//for(let i; i<=btn.length;i++){
-  btn1.addEventListener('click',function(e){
-    // aタグのデフォルトの機能を停止する
-    e.preventDefault();
-    // モーダルとオーバーレイにactiveクラスを付与する
-    modal1.classList.add('active');
-    overlay.classList.add('active');
-    // モダルの閉じるボタンをクリックしたら、モダルとオーバーレイのactiveクラスを外す
-closeBtn.addEventListener('click', function(){
-  modal1.classList.remove('active');
-  overlay.classList.remove('active');
+// $(".demo .demo__circle").click(function () {
+//   $(".modal").fadeIn()
+//   $(".modal").css("display", "flex")
+
+//   $("#popup").attr("src", src);
+//   $(".modal__caption").text(cap);
+// })
+// $(".modal__section .modal__close").click(function () {
+//   $(".modal").css("display", "none")
+// })
+
+const modalBtns = document.querySelectorAll(".container__toggle");
+modalBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    var modal = btn.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  };
+});
+const closeBtns = document.querySelectorAll(".modal__close");
+closeBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    var modal = btn.closest('.modal');
+    modal.style.display = "none";
+  };
 });
 
-// オーバーレイをクリックしたら、モダルとオーバーレイのactiveクラスを外す
-overlay.addEventListener('click', function() {
-  modal1.classList.remove('active');
-  overlay.classList.remove('active');
-});
-})
-// btn1.addEventListener('click', function(e){
-
-//   e.preventDefault();
-//   modal1.classList.add('active');
-//   overlay.classList.add('active');
-// });
-// closeBtn.addEventListener('click', function(){
-//   modal1.classList.remove('active');
-//   overlay.classList.remove('active');
-// });
-// overlay.addEventListener('click', function() {
-//   modal1.classList.remove('active');
-//   overlay.classList.remove('active');
-// });
-//
-//
+window.onclick = function (event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+};
